0af8f959c97cbf5edfbe5e89329bc7627180b34f
lauratb <laura.t.boyer@gmail.com>
2013-08-29 14:26:24 -0700

    added further notes to function

--- quick-world-map/add-continents.r
+++ quick-world-map/add-continents.r
@@ -1,11 +1,14 @@
 ## add-continents.r
-## Defines function add.continents() which adds a low-level outline of the world's continents to an existing plot
+## Defines function add.continents() which adds a quick, low-level
+## outline of the world's continents to an existing plot
+## (i.e. no need to fuss around with special map classes,
+## but the resulting outline is coarse)
 ## -------------------------------------------------------
 ## Author: Laura Tremblay-Boyer (l.boyer@fisheries.ubc.ca)
 ## Written on: August 29, 2013
 ## Time-stamp: <2013-08-29 14:17:45 Laura>
 
-add.continents <- function(lwdd=2) {
+add.continents <- function(...) {
 
 ## Extract data for continents
 ## Datasets rmp and gwm from now defunct package sspline
@@ -16,37 +19,36 @@ add.continents <- function(lwdd=2) {
         gwm <<- read.table("gwm.txt",header=TRUE)
         setwd(scd)} # back to original WD
 
-    ind1 <- rmp$inc[-1]
-    gwm$ID <- rep(1:length(ind1), ind1)
+    ind1 <- rmp$inc[-1] # rmp gives the indices for individual polygons in gwm
+    gwm$ID <- rep(1:length(ind1), ind1) # assign polygon ID to each lat/lon
     gwm.split <- split(gwm, gwm$ID)
 
-    # only plot polygon that are in the current plot frame
+    # only plot polygons that are in the current plot frame
+    # as defined by par("usr")
+
     check.if.in <- function(poly,plus360=0) {
+
         pu <- par("usr")
 
+        # include polygon is any edge occurs in the current plot
         p.in <- all(any((poly$lon + plus360) %between% pu[1:2]),
               any(poly$lat %between% pu[3:4]))
 
+        # if the polygon is in, return coordinates and add NA to create
+        # a cut when the lines() function is used
+        # (could also loop through polygons, but this is WAY faster)
         if(p.in) {return(cbind(c(poly$lon+plus360,NA), c(poly$lat,NA)))}
     }
 
-
+    # test if polygons are in:
     poly.in <- lapply(gwm.split, check.if.in)
     pmat <- do.call(rbind, poly.in)
+    # test if polygons are in using shifted longitudes
+    # (thank you South Pacific)
     poly.in360 <- lapply(gwm.split, check.if.in, plus360=360)
     pmat360 <- do.call(rbind, poly.in360)
 
-    lines(pmat)
-    lines(pmat360)
-
-    # old version
-    # Prepare data
-#    divs=cumsum(rmp)+1; ldiv=1:(nrow(divs)-1)
-
-    # Add lines on both sides of the 360 degree line
-#    dmm=sapply(ldiv,
-#    function(x) lines(gwm[divs$inc[x]:(divs$inc[x+1]-1),1:2 ], lwd=lwdd))
-#    dmm=sapply(ldiv,
-#    function(x) lines(360+gwm[divs$inc[x]:(divs$inc[x+1]-1),1],
-#                      gwm[divs$inc[x]:(divs$inc[x+1]-1),2], lwd=lwdd))
+    # ... now add lines to current plot
+    lines(pmat, ...)
+    lines(pmat360, ...)
 }
